f995a038e1746ea9fde6158960eb02b3ab308152
param071998 <param071998@gmail.com>
2023-09-11 22:30:48 +0530

    second commit

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require ('express');
 const mongoose = require ('mongoose');
 const routes = require('./routes');
 
-const mongoString ="mongodb+srv://param071998:WPTeDR44gh5twD15@cluster0.e6vscnw.mongodb.net";
-//  process.env.DATABASE_URL;
+const mongoString = process.env.DATABASE_URL;
 const app = express();
 
 app.use(express.json());
